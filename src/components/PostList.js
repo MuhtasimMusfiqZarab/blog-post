017c70d4035952c641d59239aa8cef798fea3341
zarab <musfiqzarab@iut-dhaka.edu>
2019-02-04 19:55:44 +0600

    list building

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,18 +8,38 @@ class PostList extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
+    //console.log(this.props.posts);
     return (
       <div>
-        <div>PostList</div>
+        <div className="ui relaxed divided list">{this.renderList()}</div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {};
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
 
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostList);
